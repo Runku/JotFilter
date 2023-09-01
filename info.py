6243e7ae32f3ce7e2fc3054218034a58ec65c433
Jot43 <139986366+Jot43@users.noreply.github.com>
2023-09-01 14:50:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -16,6 +16,7 @@ SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
+ADV_GRP = environ['ADV_GRP']
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
